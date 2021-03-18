470e64605ca9d21c9f6bc607fe92f386ded692d1
timis-adina <72039206+timis-adina@users.noreply.github.com>
2021-03-18 20:57:53 +0100

    Update BookShop.java

--- src/main/java/books/BookShop.java
+++ src/main/java/books/BookShop.java
@@ -1,5 +1,7 @@
 package books;
 
+import java.util.List; 
+
 public class BookShop {
 
     private final String name;
@@ -18,7 +20,51 @@ public class BookShop {
      * @return the cost in euro with the discount
      */
     public double cost(int[] books){
-        //TODO complete
-        return 0.0;
+        public double cost(int[] books){
+            double []ReductionRate = {1, 0.93, 0.86, 0.72, 0.65}; 
+            int []Reduction = new int[5]; 
+            
+            public Double DeterminePrice(List<Integer> basket) {
+                if (basket == null || basket.isEmpty()) {
+                    return 0.0;
+                }
+                int []booksInOrder = DetermineBooksInOrder(basket); 
+                DetermineReduction(booksInOrder); 
+                bestReduction();
+                double price = 0.0; 
+                for (int i = 0; i<Reduction.length;i++) {
+                    price += (8 * (i+1) * Reduction[i]) * ReductionRate[i]; 
+                }
+                return price; 
+            }
+            protected void bestReduction() {
+                while (Reduction[2]>0 && Reduction[4]>0) {
+                    Reduction[2]--; 
+                    Reduction[4]--; 
+                    Reduction[3] += 2; 
+                }
+            }
+            protected void DetermineReduction(int[]booksInOrder) {
+                if (booksInOrder == null) {
+                    return; 
+                }
+                int otherThanZero = 0; 
+                for (int i = 0; i < booksInOrder.length; i++) {
+                    if (booksInOrder[i]>0) {
+                        otherThanZero++; 
+                        booksInOrder[i]--; 
+                    }
+                }
+                if (otherThanZero > 0) {
+                    Reduction[otherThanZero - 1] += 1; 
+                    DetermineReduction(booksInOrder);
+                }
+            }
+            private int[]DetermineBooksInOrder(List<Integer>basket){
+                int[] result = new int[5]; 
+                for (int book : basket) {
+                    result[book - 1]++; 
+                }
+                return result;
     }
 }
